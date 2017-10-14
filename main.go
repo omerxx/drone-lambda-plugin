67199dd2e34a43dbc7a7a6d97d5055f45056c335
omerxx <omerxx@gmail.com>
2017-10-14 21:51:46 +0300

    Strcuture with ENV VARS as params

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go/service/lambda"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws"
+)
+
+
+func main() {
+    svc := lambda.New(session.New(&aws.Config{
+        Region: aws.String("us-east-1"),
+    }))
+
+    input := &lambda.UpdateFunctionCodeInput{
+        FunctionName:    aws.String(os.GetEnv("PLUGIN_FUNCTION_NAME")),
+        Publish:         aws.Bool(true),
+        S3Bucket:        aws.String(os.GetEnv("PLUGIN_S3_BUCKET")),
+        S3Key:           aws.String(os.GetEnv("PLUGIN_PATH_PREFIX") + os.GetEnv("PLUGIN_FILE_NAME"))
+    }
+
+    result, err := svc.UpdateFunctionCode(input)
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok {
+            switch aerr.Code() {
+                case lambda.ErrCodeServiceException:
+                    fmt.Println(lambda.ErrCodeServiceException, aerr.Error())
+                case lambda.ErrCodeResourceNotFoundException:
+                    fmt.Println(lambda.ErrCodeResourceNotFoundException, aerr.Error())
+                case lambda.ErrCodeInvalidParameterValueException:
+                    fmt.Println(lambda.ErrCodeInvalidParameterValueException, aerr.Error())
+                case lambda.ErrCodeTooManyRequestsException:
+                    fmt.Println(lambda.ErrCodeTooManyRequestsException, aerr.Error())
+                case lambda.ErrCodeCodeStorageExceededException:
+                    fmt.Println(lambda.ErrCodeCodeStorageExceededException, aerr.Error())
+                default:
+                    fmt.Println(aerr.Error())
+            }
+        } else {
+            // Print the error, cast err to awserr.Error to get the Code and
+            // Message from an error.
+            fmt.Println(err.Error())
+        }
+        return
+    }
+
+    fmt.Println(result)
+}
+
